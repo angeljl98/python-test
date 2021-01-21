44eeb7325e958b845fd7eba6798e1c4a0bb5d753
angeljl98 <30377491+angeljl98@users.noreply.github.com>
2021-01-21 16:22:28 -0400

    Initial commit

---
+++ largest_number.py
@@ -0,0 +1,8 @@
+# code to find largest integer in a array
+# version 1.0
+def find_largest(numbers):
+    array_read=numbers
+    n=len(array_read)
+    m=max(array_read)
+    return m
+print(find_largest([1, -28, 88, 200, 7]))  # 200
---
+++ print_abecedario.py
@@ -0,0 +1,6 @@
+# version 1.1
+from string import ascii_letters
+print(ascii_letters)
+import string
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
